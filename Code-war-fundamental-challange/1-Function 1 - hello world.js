ff04ebcdf6a75ef173d04ddbafcfaae788f324eb
ahadizulfida <57039534+ahadizulfida@users.noreply.github.com>
2024-03-17 00:54:48 +0700

    upload file challenge

---
+++ Code-war-fundamental-challange/1-Function 1 - hello world.js
@@ -0,0 +1,12 @@
+// Code war fundamental challenge
+// 1
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+
+// function greeting(){
+//     return 'hello world!'
+// }
+
+const greeting = () => 'hello world!'
+
+console.log(greeting());
---
+++ Code-war-fundamental-challange/2-makeUpperCase.js
@@ -0,0 +1,15 @@
+// Code war fundamental challenge
+// 2
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+
+// function makeUpperCase(str) {
+//     // Code here
+//     return str.toUpperCase()
+//   }
+
+const makeUpperCase = str => str.toUpperCase();
+
+//toUpperCase merupakan built in method untuk string yang akan mengubah nilai string menjadi huruf besar (uppercase)
+
+console.log(makeUpperCase("hello"));
